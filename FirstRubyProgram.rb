fc8689a57ebc10eba2c22a660c54737c7a828236
SureshDamodaran <35876555+SureshDamodaran@users.noreply.github.com>
2018-01-29 00:08:03 +0530

    Add files via upload

--- FirstRubyProgram.rb
+++ FirstRubyProgram.rb
@@ -5,3 +5,7 @@ puts "**********My First Ruby Program***************"  # using double quotes
 print "\n"
 puts '**********************************************'  # using single quotes
 puts "\t\tEnd of Program\n\n"
+
+# Assigning values to multiple variables in single line
+a,b,c = 10,15,20
+print "A:a=", a , ",b=", b , ",c=", c, "\n"
