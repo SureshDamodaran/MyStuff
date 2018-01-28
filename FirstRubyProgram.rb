4dc52231163028ec09e20e714e05150bcc38597a
SureshDamodaran <35876555+SureshDamodaran@users.noreply.github.com>
2018-01-28 23:54:53 +0530

    Add files via upload

---
+++ FirstRubyProgram.rb
@@ -0,0 +1,7 @@
+
+print "\n"
+puts "**********************************************"
+puts "**********My First Ruby Program***************"  # using double quotes
+print "\n"
+puts '**********************************************'  # using single quotes
+puts "\t\tEnd of Program\n\n"
